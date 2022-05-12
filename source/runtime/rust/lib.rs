75d6188c5bef567b887eca05dfd879e021dc7ea3
Anthony Weathersby <acweathersby.codes@gmail.com>
2022-05-12 12:14:26 -0600

    Make  constant

--- source/runtime/rust/lib.rs
+++ source/runtime/rust/lib.rs
@@ -9,6 +9,6 @@ pub use lazy_static::lazy_static;
 pub use recognizer::*;
 use utf8::*;
 
-pub fn get_version_string() -> &'static str {
+pub const fn get_version_string() -> &'static str {
     "HCToolkit Runtime Library 2022 v0.0.1"
 }
