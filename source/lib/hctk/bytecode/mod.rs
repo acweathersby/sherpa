4df796563bba2ab80990a3abfcc09dea1f42155d
Anthony Weathersby <acweathersby.codes@gmail.com>
2022-07-03 22:28:54 -0600

    Move test to module file

--- source/lib/hctk/bytecode/mod.rs
+++ source/lib/hctk/bytecode/mod.rs
@@ -25,7 +25,8 @@ pub struct BytecodeOutput
 
 impl BytecodeOutput
 {
-    pub fn get_inverted_state_lookup(&self) -> BTreeMap<u32, String>
+    /// Returns the inverted version of `name_to_offset`
+    pub fn get_offset_to_name(&self) -> BTreeMap<u32, String>
     {
         self.name_to_offset
             .iter()
@@ -61,5 +62,49 @@ pub fn compile_bytecode(
         bytecode,
         states,
         name_to_offset: state_lookups,
+    }
+}
+
+#[cfg(test)]
+mod byte_code_creation_tests
+{
+
+    use std::collections::HashMap;
+
+    use crate::bytecode::compile::compile_ir_state_to_bytecode;
+    use crate::bytecode::constants::default_get_branch_selector;
+    use crate::debug::compile_test_grammar;
+    use crate::grammar::data::ast::ASTNode;
+    use crate::grammar::get_production_id_by_name;
+    use crate::grammar::parse::compile_ir_ast;
+    use crate::intermediate::state::generate_production_states;
+
+    #[test]
+    pub fn test_produce_a_single_ir_ast_from_a_single_state_of_a_trivial_production(
+    )
+    {
+        let grammar = compile_test_grammar("<> A > \\h");
+
+        let prod_id = get_production_id_by_name("A", &grammar).unwrap();
+
+        let result = generate_production_states(&prod_id, &grammar);
+
+        println!("{:#?}", result);
+
+        let state = result[0].get_code();
+
+        let result = compile_ir_ast(Vec::from(state.as_bytes()));
+
+        println!("{:#?}", result);
+
+        assert!(result.is_ok());
+
+        let result = compile_ir_state_to_bytecode(
+            &result.unwrap(),
+            default_get_branch_selector,
+            &HashMap::new(),
+        );
+
+        println!("{:#?}", result);
     }
 }
