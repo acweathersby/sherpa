dce58dd8a03156335c409d3b88d28d4f4d36e93e
Anthony Weathersby <acweathersby.codes@gmail.com>
2022-11-25 14:56:48 -0700

    Change `_prime` to `'`

--- source/lib/hctk-core/grammar/compile.rs
+++ source/lib/hctk-core/grammar/compile.rs
@@ -1330,8 +1330,8 @@ pub fn convert_left_recursion_to_right(
   let non_bodies =
     bodies.iter().filter_map(|(i, b)| if *b { None } else { Some(i) }).collect::<Vec<_>>();
 
-  let a_prime_prod_name = format!("{}_prime", a_prod.name);
-  let a_prime_prod_guid_name = format!("{}_prime", a_prod.guid_name);
+  let a_prime_prod_name = format!("{}'", a_prod.name);
+  let a_prime_prod_guid_name = format!("{}'", a_prod.guid_name);
   let a_prime_prod_id = ProductionId::from(&a_prime_prod_guid_name);
   let a_prime_prod = Production {
     id: a_prime_prod_id,
