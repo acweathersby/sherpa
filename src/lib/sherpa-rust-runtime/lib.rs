65f3be04aed1f90fd169651d581f732ade341402
Anthony Weathersby <acweathersby.codes@gmail.com>
2023-08-22 10:21:44 -0600

    Remove unused features

--- src/lib/sherpa-rust-runtime/lib.rs
+++ src/lib/sherpa-rust-runtime/lib.rs
@@ -2,8 +2,6 @@
 //!
 //! The Sherpa Runtime Library provides the runtime functions and types
 //! necessary to execute Sherpa parsers in Rust.
-#![feature(const_mut_refs)]
-#![feature(const_for)]
 
 pub mod bytecode;
 pub mod llvm_parser;
