216c79ed6ee1ebf3e166f38a57e9bebf9be4e4f5
Anthony Weathersby <acweathersby.codes@gmail.com>
2023-12-28 09:30:35 -0700

    Update radlr-build interface

--- src/lib/radlr-build/lib.rs
+++ src/lib/radlr-build/lib.rs
@@ -1,11 +1,11 @@
-mod targets;
-
-use std::path::Path;
-
 use radlr_core::RadlrGrammar;
 pub use radlr_core::RadlrResult;
+use std::path::Path;
 
-pub enum RadlrBuildTarget {
+mod targets;
+
+#[derive(Clone, Copy)]
+pub enum TargetLanguage {
   Rust,
   TypeScript,
   JavaScript,
@@ -14,47 +14,98 @@ pub enum RadlrBuildTarget {
   Llvm,
 }
 
+#[derive(Clone, Copy)]
+pub enum ParserType {
+  /// Builds a bytecode parser to be run by the Radlr bytecode interpreter
+  /// of target langauge.
+  Bytecode,
+  /// Build a binary bytecode parser for the target machine. Not all target
+  /// languages support this parsing mode.
+  Binary,
+}
+
 /// Configurations for artifacts built by radlr-build.
 #[derive(Clone, Copy)]
 pub struct BuildConfig<'a> {
   pub ast_struct_name: &'a str,
+
+  /// Create an ast artifact file in the target language based on `:ast`
+  /// definitions found in grammar files.
+  ///
+  /// Defaults to true
   pub build_ast: bool,
+
   pub include_debug_symbols: bool,
+
+  /// Directory to place library type artifacts including
+  /// binary files and shared/static libraries.
+  ///
+  /// Defaults to the parent dir of `root_grammar_path` if lib out is not
+  /// set.
+  pub lib_out: &'a Path,
+
+  /// Directory to place source type artifacts such as
+  /// generated parser and AST code files.
+  ///
+  /// Defaults the parent dir of `root_grammar_path` if lib out is not
+  /// set.
+  pub source_out: &'a Path,
+
+  /// The type of parser to construct.
+  ///
+  /// Defaults to Bytecode.
+  pub parser_type: ParserType,
+
+  /// Path of the root grammar file.
+  root_grammar_path: &'a Path,
+
+  /// Paths to search for `radlr` input files.
+  include_paths: &'a [&'a Path],
 }
 
-const DEFAULT_AST_NAME: &str = "AST_TestNode";
-
-impl<'a> Default for BuildConfig<'a> {
-  fn default() -> Self {
+impl<'a> BuildConfig<'a> {
+  pub fn new(root_grammar: &'a Path) -> Self {
     BuildConfig {
-      ast_struct_name: DEFAULT_AST_NAME,
+      ast_struct_name: "AST_TestNode",
+      lib_out: &root_grammar.parent().unwrap(),
+      source_out: &root_grammar.parent().unwrap(),
       build_ast: true,
       include_debug_symbols: false,
+      root_grammar_path: root_grammar,
+      parser_type: ParserType::Bytecode,
+      include_paths: &[],
     }
   }
 }
 
-/// Build a Radlr parser from a set grammar files.
-pub fn fs_build(
-  root_grammar: &Path,
-  output_dir: &Path,
-  build_config: BuildConfig,
-  parser_config: radlr_core::ParserConfig,
-  target: RadlrBuildTarget,
-) -> RadlrResult<()> {
-  let db = RadlrGrammar::new().add_source(root_grammar)?.build_db(root_grammar, parser_config)?;
+/// Build a Radlr parser from a grammar file
+pub fn fs_build(build_config: BuildConfig, parser_config: radlr_core::ParserConfig, target: TargetLanguage) -> RadlrResult<()> {
+  let BuildConfig { lib_out, source_out, root_grammar_path, .. } = build_config;
+
+  let db = RadlrGrammar::new().add_source(root_grammar_path)?.build_db(root_grammar_path, parser_config)?;
+
+  std::fs::create_dir_all(lib_out)?;
+  std::fs::create_dir_all(source_out)?;
 
   match target {
-    RadlrBuildTarget::Rust => {
-      targets::rust::build(&db, build_config, parser_config, output_dir)?;
+    TargetLanguage::Rust => {
+      targets::rust::build(&db, build_config, parser_config)?;
     }
-    RadlrBuildTarget::TypeScript => {
-      targets::typescript::build(&db, build_config, parser_config, output_dir)?;
+    TargetLanguage::TypeScript => {
+      targets::typescript::build(&db, build_config, parser_config)?;
     }
-    RadlrBuildTarget::JavaScript => {}
-    RadlrBuildTarget::Cpp => {}
-    RadlrBuildTarget::C => {}
-    RadlrBuildTarget::Llvm => {}
+    TargetLanguage::JavaScript => {
+      todo!("Build Javascript: Not yet supported")
+    }
+    TargetLanguage::Cpp => {
+      todo!("Build Cpp: Not yet supported")
+    }
+    TargetLanguage::C => {
+      todo!("Build C: Not yet supported")
+    }
+    TargetLanguage::Llvm => {
+      todo!("Build Llvm: Not yet supported")
+    }
     _ => {}
   }
 
@@ -66,6 +117,12 @@ fn builds_basic_grammar() -> RadlrResult<()> {
   let root = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).canonicalize()?;
   let path = root.join("test_grammar.radlr");
   let output = root.join("build");
-  fs_build(&path, &output, Default::default(), Default::default(), RadlrBuildTarget::TypeScript)?;
+
+  let mut build_config = BuildConfig::new(&path);
+  build_config.source_out = &output;
+  build_config.lib_out = &output;
+
+  fs_build(build_config, Default::default(), TargetLanguage::Rust)?;
+
   Ok(())
 }
--- src/lib/radlr-build/targets/common/mod.rs
+++ src/lib/radlr-build/targets/common/mod.rs
@@ -2,9 +2,9 @@ use crate::BuildConfig;
 use radlr_ascript_beta::*;
 use radlr_core::{proxy::OrderedMap, *};
 use radlr_formatter::{Formatter, FormatterContext, FormatterResult, ToValue, Value};
-use std::{collections::HashMap, fs::*, io::Write, path::Path};
+use std::{collections::HashMap, fs::*, io::Write};
 
-pub fn build_ascript_ast(
+pub fn build_ast_source(
   db: &RadlrDatabase,
   script: &str,
   ast_path: std::path::PathBuf,
@@ -20,4 +20,58 @@ pub fn build_ascript_ast(
 
     adb.format(script, parser, 100, build_config.ast_struct_name)?.flush()?;
   })
+}
+
+pub fn build_parser_states(db: &RadlrDatabase, parser_config: ParserConfig) -> Result<RadlrIRParser, RadlrError> {
+  let states = db.build_states(parser_config)?;
+  let parser = states.build_ir_parser(true, false)?;
+  Ok(parser)
+}
+
+pub fn build_parser_source(
+  db: &RadlrDatabase,
+  parser_script: &str,
+  bytecode: radlr_rust_runtime::types::BytecodeParserDB,
+  binary_out_path: std::path::PathBuf,
+  parser_out_path: std::path::PathBuf,
+) -> Result<(), RadlrError> {
+  let parser = OpenOptions::new().append(false).truncate(true).write(true).create(true).open(&parser_out_path)?;
+  let s_store = db.get_internal().string_store();
+  let mut ctx: FormatterContext = FormatterContext::new("RustForm", s_store.clone());
+  let states_lu = bytecode
+    .state_to_token_ids_map
+    .iter()
+    .map(|(id, val)| (id.to_string(), val.clone().into_iter().map(|v| -> Value { v.into() }).collect::<Vec<_>>()))
+    .collect::<HashMap<_, _>>();
+  let nonterm_name_to_id =
+    bytecode.nonterm_name_to_id.iter().map(|(id, val)| (id.clone(), Value::from(*val))).collect::<OrderedMap<_, _>>();
+  let token_id_to_str =
+    bytecode.token_id_to_str.iter().map(|(id, val)| (id.to_string(), val.into_val(s_store))).collect::<OrderedMap<_, _>>();
+  let nonterm_id_to_address = bytecode
+    .nonterm_id_to_address
+    .iter()
+    .map(|(id, val)| (id.to_string(), (*val).into_val(s_store)))
+    .collect::<OrderedMap<_, _>>();
+  let mut token_maps: std::collections::BTreeMap<Vec<u32>, usize> = OrderedMap::new();
+  let state_to_token_ids_map = bytecode
+    .state_to_token_ids_map
+    .iter()
+    .map(|(state, ids)| {
+      let index = token_maps.len();
+      let id = *token_maps.entry(ids.clone()).or_insert(index);
+      ((*state).to_string(), id)
+    })
+    .collect::<OrderedMap<_, _>>();
+  let token_maps = token_maps.iter().map(|(v, k)| (k.to_string(), v.clone())).collect::<OrderedMap<_, _>>();
+  ctx.set_val("ir_token_lookup", Value::Obj(&states_lu));
+  ctx.set_val("binary_path", binary_out_path.intern(db.get_internal().string_store()).into());
+  ctx.set_val("nonterm_name_to_id", Value::Obj(&nonterm_name_to_id));
+  ctx.set_val("token_id_to_str", Value::Obj(&token_id_to_str));
+  ctx.set_val("nonterm_id_to_address", Value::Obj(&nonterm_id_to_address));
+  ctx.set_val("state_to_token_ids_map", Value::Obj(&state_to_token_ids_map));
+  ctx.set_val("token_maps", Value::Obj(&token_maps));
+  ctx.max_width = 100;
+  let f: Formatter = FormatterResult::from(parser_script).into_result()?;
+  f.write_to_output(&mut ctx, parser)?.flush()?;
+  Ok(())
 }
